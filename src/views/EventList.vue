cc75dfb8ed14daec2e15bedd42284bd8f8cbc00b
ziganikolic <ziganikolicpc@gmail.com>
2021-12-19 19:08:59 +0100

    comit

---
+++ src/views/EventList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="events">
+    <h1>Events for good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue';
+import EventServices from '@/services/EventServices.js'
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null
+    }
+  },
+  created(){
+    EventServices.getEvents()
+    .then(response =>{
+      this.events = response.data
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+};
+</script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
